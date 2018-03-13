83af071fb05771174165d082ef005bdfb05e4762
eyramm <amedzor13@gmail.com>
2018-03-13 08:48:16 +0000

    Update sidebar.blade.php

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,21 +1,21 @@
 <div class="col-md-3 sidebar">
     <div class="panel panel-default panel-flush">
-        <div class="panel-heading">
+        <!-- <div class="panel-heading">
             Sidebar
             
-        </div>
+        </div> -->
 
         <div class="panel-body">
             <ul class="nav" role="tablist">
 
-                    <router-link to='/passport-clients' tag='li'>
+                    <!-- <router-link to='/passport-clients' tag='li'>
                         <a>Passport Client</a>
                     </router-link>
 
                     
                     <router-link to='/passport-personal-access-tokens' tag='li'>
                         <a>Passport Personal Access Tokens</a>
-                    </router-link>
+                    </router-link> -->
 
                     <router-link to='/field-form' tag='li'>
                         <a>Collection</a>
